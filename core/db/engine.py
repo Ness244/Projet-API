870e88c76ef620343477384f7162a28988226b15
milkshake308 <milkshake.2734@gmail.com>
2024-05-09 15:49:44 +0200

    (feat) admin creation if no super admin in db

--- core/db/engine.py
+++ core/db/engine.py
@@ -4,6 +4,7 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from core.db.models import Base, User, Syslog
+from core.security import get_password_hash
 import config
 
 DATABASE_URL = f"mysql+mysqlconnector://{config.DATABASE_USER}:{config.DATABASE_PASSWORD}@{config.DATABASE_HOST}/{config.DATABASE_NAME}"
@@ -23,4 +24,22 @@ def get_db():
 SessionDep = Annotated[Session, Depends(get_db)]
 
 def init_superadmin():
-    # TODO: perfom admin crud and print new admin password
+    session = sessionmaker(autoflush=False, bind=engine)()
+
+    is_admin = session.query(User).filter(User.is_superuser == True).first()
+    if is_admin:
+        return True
+
+    default_password = get_password_hash('admin')
+    admin_obj = {
+        "full_name": "Administrator",
+        "username":"admin",
+        "is_superuser": True,
+        "password": default_password
+    }
+    admin_obj = User(**admin_obj)
+    session.add(admin_obj)
+    session.commit()
+    session.refresh(admin_obj)
+
+    return admin_obj
