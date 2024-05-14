abc4a4ce68dc76b0c1d3718d48c666e2941082e6
akyenneges <kdares1@myges.fr>
2024-05-14 20:41:00 +0200

    fix add cors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from api.main import api
 from core.db.engine import init_superadmin
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(api, prefix='/api')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 init_superadmin()
